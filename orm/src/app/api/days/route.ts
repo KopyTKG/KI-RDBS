0574698db8fd2af188ba5272c4a951fad64f2666
Martin Kopecký <kopecky@thekrew.app>
2024-01-16 11:15:28 +0100

    orm update

---
+++ orm/src/app/api/days/route.ts
@@ -0,0 +1,7 @@
+import prisma from '../../../../prisma/client'
+
+export const dynamic = 'force-dynamic'
+
+export async function GET() {
+  return new Response(JSON.stringify(await prisma.days.findMany()))
+}
